bf7eb96b22370aa34238079a7fd5227eea93302a
Aishwarya.V <aishwaryav370@gmail.com>
2026-07-08 00:26:38 +0530

    Add files via upload

--- practice que01.py
+++ practice que01.py
@@ -1,21 +1,44 @@
-#store following word meanings in a python dictionary:
-#table = "a piece of furniture","list of facts and figure"
-#cat = "a small animal "
+#01. print number from 1 to 100 
+i = 1
+while i <=100:
+    print(i)
+    i +=1 
+    
+#02. print numbers from 100 to 1 
+i = 100 
+while i >= 1:
+    print(i)
+    i -= 1
+    
+#03. Print the multiplication table of number n
+n = int(input("Enter the number: "))
+i = 1 
+while i<=10:
+    print(n*i)
+    i += 1
+    
+#04.print the elemet of the following list using a loop 
+#[1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
 
-dict= {
-    "table" : ["a piece of furniture", "list of facts and figures"],
-    "cat" : "a small animal"
-}
+nums = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
 
-print(dict)
+i = 0
+while i < len(nums):
+    print(nums[i])
+    i += 1
+    
+#05.Search for a number x in this tuple using loop:(1, 4, 9, 16, 25, 36, 49, 64, 81, 100)
 
-#you are given a list of sub for students.Assume one classroom is required for 1 subject.How many classroom are needed by all students.
-# "python","java", "C++","python", "javascript","java", "python", " java", "C++", "C"
+nums = (1, 4, 9, 16, 25, 36, 49, 64, 81, 100)
 
-subjects = {"python","java", "C++","python",
-            "javascript","java", "python", "java", "C++", "C"
-   }
+x = 36 
+i = 1 
+while i < len(nums):
+    if(nums[i] == x):
+        print("FOUND AT INDEX", i)
+    i += 1 
 
-print(len(subjects))
+
+
 
 
