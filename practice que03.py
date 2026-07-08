2c83ff869fe431c811d7d0938426af15b0d3f6fd
Aishwarya.V <aishwaryav370@gmail.com>
2026-07-09 01:20:33 +0530

    Add files via upload

---
+++ practice que03.py
@@ -0,0 +1,18 @@
+#WAP to find the sum of first n numbers (using while)
+n = 5 
+
+sum = 0
+for i in range(1, n+1):
+    sum += i
+    
+print("total sum = ", sum)
+
+#WAP to find the factorial of first n numbers 
+n = 6
+sum = 0
+i = 1
+while i <= n:
+    sum += i 
+    i +=1
+    
+    print("Total sum =", sum)
