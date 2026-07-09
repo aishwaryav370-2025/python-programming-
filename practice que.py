943d75ed2109c062b5faf34423c363d24884e230
Aishwarya.V <aishwaryav370@gmail.com>
2026-07-10 00:14:41 +0530

    Add files via upload

--- practice que.py
+++ practice que.py
@@ -1,45 +1,16 @@
-#WAP to ask the user to enter names of their 3 favorite movies and store them in a list
-movies = []
-mov1 = input("Enter 1st movie: ")
-mov2 = input("Enter 2nd movie: ")
-mov3 = input("Enter 3rd movie: ")
+#WAP to print the len of the list (list is the parameter)  
 
-movies.append(mov1)
-movies.append(mov2)
-movies.append(mov3)
+cities = ("delhi", "gurgaon", "noida", "pune", "mumbai", "bengaluru")
+heroes = ("thor", "yash", " captain america", "shakti man")
 
-print(movies)
-
-#or 
-movies = []
-movies.append(input("Enter 1st movie: "))
-movies.append(input("Enter 2nd movie: "))
-movies.append(input("Enter 3rd movie: "))
-
-print(movies)
-
-#WAP to check if a list contains a palindrome of element.
-list1 = [1, 2, 1]
-
-
-copy_list1 = list1.copy()
-copy_list1.reverse()
-
-if(copy_list1 == list1):
-    print("Pallindrome")
-else:
-    print("Not pallidrome ")
+def print_len(list):
+    print(len(list))
     
-#WAP to count the number of students with the "A" grade in the following tuple 
-#["c","D", "A", "A","B", "B", "A"]
+print_len(cities)
+print_len(heroes)
 
-grade = ["c","D", "A", "A","B","B" ,"A"]
-print(grade.count("A"))
+#WAF to print the elements of a list in a single line (list is the parameter)
 
-#Store the above values in a list and sort them from "A" to "D"
-
-list = ["c","D", "A", "A","B", "B", "A"]
-list.sort()
-print(list)
-
-
+movies = ("toxic", "kgf", "googly", "lucky")
+print(movies[0], end=" ")
+print(movies[1], end=" ")
