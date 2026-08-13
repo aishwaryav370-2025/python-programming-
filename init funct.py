81e3c393ec9e6d9a1b4c93e5438aab58c56ff218
Aishwarya.V <aishwaryav370@gmail.com>
2026-08-13 20:08:57 +0530

    Add files via upload

---
+++ init funct.py
@@ -0,0 +1,9 @@
+#constructor function 
+
+class Student:
+    name = "Aishwarya"
+    def __init__(self):
+        print(self)
+        print("adding new student in Database..")
+        
+s1 = Student()
