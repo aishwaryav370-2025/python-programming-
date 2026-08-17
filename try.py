52fe6a4b0802ce90885a171c1dc13c9c8fb6fad1
Aishwarya.V <aishwaryav370@gmail.com>
2026-08-17 20:54:42 +0530

    Add files via upload

---
+++ try.py
@@ -0,0 +1,17 @@
+while(True):
+    print("Press q to quit")
+    a = input("Enter a number: ")
+    if a == 'q':
+        break
+    try:
+        a = int(a)
+        if a > 6:
+            print("You entered a number greater than 6")
+            
+    except Exception as e:
+        print(e)
+        
+print("Thanks for playing this game ")
+
+    
+    
