9a9d7b33aa19d75f5889c79d22874278c83db31f
Aishwarya.V <aishwaryav370@gmail.com>
2026-08-17 20:54:21 +0530

    Add files via upload

---
+++ python calculator.py
@@ -0,0 +1,9 @@
+a = 10
+b = 45
+
+print("addition:", a + b)
+print("substraction:", a - b)
+print("multiplication:", a * b)
+print("division:", a / b)
+print("Exponentional:", a ** b)
+print("Module:", a % b)
